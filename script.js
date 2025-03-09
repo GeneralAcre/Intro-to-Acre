86b5fd35acda0e6bf546b74266a942abdfbc3489
Sanpaphat Porntongprasert <sanpaphat14@gmail.com>
2025-03-09 23:31:07 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const SocialMedia = document.getElementById("Social-Media");
+
+SocialMedia.addEventListener("click", () => {
+    window.open("https://www.linkedin.com/in/sanpaphat-porntongprasert-0b239b321/");
+});
+
